cc44b6f2f348b1182f8c493291fcd4293b1d7a3e
Shahraz Mirza <129364236+shahrazmirza@users.noreply.github.com>
2024-01-12 02:18:43 +1100

    Update Whatsapp.jsx

--- app/components/Whatsapp.jsx
+++ app/components/Whatsapp.jsx
@@ -26,6 +26,7 @@ const Whatsapp = () => {
     <div>
       <Link
       href="https://wa.me/+61403320350"
+      target="_blank"
       className={`fixed md:w-14 md:h-14 w-12 h-12 text-center flex items-center justify-center bottom-7 md: right-7 bg-sky-700 text-white p-0 rounded-full transition-opacity ${
         isVisible ? 'opacity-80' : 'opacity-80'
       }`}
