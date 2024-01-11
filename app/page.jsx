95437452cd7f0f12109c4cdf53b5555cdc3da475
Shahraz Mirza <129364236+shahrazmirza@users.noreply.github.com>
2024-01-12 02:16:08 +1100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Scroll_to_top from './components/Scroll_to_top';
+import Whatsapp from './components/Whatsapp';
 import Hero from './components/Hero';
 import Services_section_one from './components/Services_section_one';
 import Services_section_two from './components/Services_section_two';
@@ -8,11 +9,13 @@ import Associations from './components/Associations';
 import Faq from './components/Faq';
 import Footer from './components/Footer';
 
+
 const page = () => {
   return (
     <div>
       <Navbar />
       <Scroll_to_top />
+      <Whatsapp />
       <Hero />
       <Services_section_one />
       <Services_section_two />
