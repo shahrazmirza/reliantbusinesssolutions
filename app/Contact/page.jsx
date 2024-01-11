2224b855e36837423e2c66a9dffdacd0c9f06141
Shahraz Mirza <129364236+shahrazmirza@users.noreply.github.com>
2024-01-12 03:08:45 +1100

    Update page.jsx

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -4,12 +4,14 @@ import Scroll_to_top from '../components/Scroll_to_top'
 import Footer from '../components/Footer'
 import { Container } from '@radix-ui/themes'
 import Contact_Form from '../components/Contact_Form'
+import Whatsapp from '../components/whatsapp'
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <Scroll_to_top />
+      <Whatsapp />
       <div className='py-5'>
         <div className='flex flex-col text-center justify-center bg-sky-50 md:pt-24 py-10 md:pb-12 px-7'>
           <p className='text-2xl/snug md:text-3xl/loose font-semibold'>
