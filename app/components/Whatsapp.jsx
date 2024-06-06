cadab5b00b586f3b1f5db8256620c33cdf83fb74
shahrazmirza <shahrazahmedmirza@live.com>
2024-06-06 23:50:24 +1000

    Commit

--- app/components/Whatsapp.jsx
+++ app/components/Whatsapp.jsx
@@ -25,14 +25,15 @@ const Whatsapp = () => {
   return (
     <div>
       <Link
-        aria-label="whatsapp contact"
         href="https://wa.me/+61403320350"
         target="_blank"
         className={`fixed md:w-14 md:h-14 w-12 h-12 text-center flex items-center justify-center bottom-7 md: right-7 bg-green-500 text-white p-0 rounded-full transition-opacity ${
           isVisible ? "opacity-80" : "opacity-100"
         }`}
       >
-        <FaWhatsapp className="md:w-6 md:h-6" />
+        <button aria-label="whatsapp contact">
+          <FaWhatsapp className="md:w-6 md:h-6" />
+        </button>
       </Link>
     </div>
   );
