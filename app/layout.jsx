b018ba0d6a72cb12a5dbf3c3813137b8d68d60d0
shahrazmirza <shahrazahmedmirza@live.com>
2024-06-07 22:33:40 +1000

    Commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,22 @@ export const metadata = {
   keywords:
     "accounting services, tax services, bookkeeping, financial planning, business accounting, personal tax, business startup accounting, NDIS plan management, grant assistance, registrations",
   author: "Reliant Business Solutions",
+  openGraph: {
+    type: "website",
+    url: "https://www.reliantbusinesssolutions.com.au",
+    title: "Reliant Business Solutions - Accounting Services Nationwide",
+    description:
+      "Discover a wide array of personal and business accounting services at Reliant Business Solutions. Get expert advice, financial planning, tax management, and more.",
+    image: "https://www.reliantbusinesssolutions.com.au/hero-image.jpg",
+  },
+  twitter: {
+    card: "summary_large_image",
+    url: "https://www.reliantbusinesssolutions.com.au",
+    title: "Reliant Business Solutions - Accounting Services Nationwide",
+    description:
+      "Discover a wide array of personal and business accounting services at Reliant Business Solutions. Get expert advice, financial planning, tax management, and more.",
+    image: "https://www.reliantbusinesssolutions.com.au/hero-image.jpg",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -24,6 +40,22 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content={metadata.author} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:image" content={metadata.openGraph.image} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:url" content={metadata.twitter.url} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta
+          name="twitter:description"
+          content={metadata.twitter.description}
+        />
+        <meta name="twitter:image" content={metadata.twitter.image} />
         <title>{metadata.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </head>
